a1520c4d7ebd5dea16fde0732742e4ec2b6524bc
Agung <agungananda17@icloud.com>
2026-07-07 23:24:02 +0800

    1) Make the exit button functional; 2) Add the pop-over for the exit button, make it contextual for Host and Player

---
+++ Sanscore/Sanscore/SanscoreiOS/Views/EndRoomView.swift
@@ -0,0 +1,18 @@
+//
+//  EndRoomView.swift
+//  Sanscore
+//
+//  Created by Agung Ananda on 07/07/26.
+//
+
+import SwiftUI
+
+struct EndRoomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EndRoomView()
+}
---
+++ Sanscore/Sanscore/SanscoreiOS/Views/LeaveRoomView.swift
@@ -0,0 +1,18 @@
+//
+//  CloseRoomView.swift
+//  Sanscore
+//
+//  Created by Agung Ananda on 07/07/26.
+//
+
+import SwiftUI
+
+struct CloseRoomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CloseRoomView()
+}
